21e84ebf459e338ed7443cc4772c2d2d0b3697e7
erai <erai@omiltem.net>
2024-04-29 15:40:27 -0400

    Verify signature

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -uex
 git pull --ff-only
+if [ git show -s --pretty='%G?' != 'G' ]; then
+  echo "Failed to verify signature" >&2
+  exit 1
+fi
 make
 rsync --delete -r build/ /app/web/
