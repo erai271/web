5dc3585947058727729acd5201ec70e4de726965
erai <erai@omiltem.net>
2024-04-29 15:43:51 -0400

    Fix sig check

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -uex
 git pull --verify-signatures --ff-only origin main
-if [ git show -s --pretty='%G?' != 'G' ]; then
+if [ "$(git show -s --pretty='%G?')" != 'G' ]; then
   echo "Failed to verify signature" >&2
   exit 1
 fi
