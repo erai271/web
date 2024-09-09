3b3ef08b61587a8e2a1c27b77bc59fa9bef957a5
erai <erai@omiltem.net>
2024-09-09 11:34:42 -0400

    Update deploy script for remote hosts

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -uex
+if [ $# -ne 1 ]; then
+  echo "Usage: ./deploy.sh <host>"
+  exit 1
+fi
+case "$1" in
+localhost) target="" ;;
+*) target="$1:" ;;
+esac
 git pull --verify-signatures --ff-only origin main
 if [ "$(git show -s --pretty='%G?')" != 'G' ]; then
   echo "Failed to verify signature" >&2
   exit 1
 fi
 make
-rsync --delete -r build/ /app/web/
+rsync --delete -vr build/ "${target}/app/web/"
